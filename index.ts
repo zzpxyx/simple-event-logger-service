c839cf355ddf16ed06654b41fbfba5bd3396d0f2
zzpxyx <zzpxyx@users.noreply.github.com>
2024-05-22 15:27:26 -0500

    Use in-memory fake database.

--- index.ts
+++ index.ts
@@ -1,10 +1,26 @@
 import express from "express";
 
-const app = express();
 const port = 3000;
+const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Hello world");
+app.use(express.json());
+
+type Event = {
+  timestamp: number;
+  event: string;
+  memo: string;
+};
+
+const db: Event[] = [];
+
+app.get("/v1/events", (_req, res) => {
+  res.status(200).json(db);
+});
+
+app.post("/v1/events", (req, res) => {
+  const event: Event = req.body;
+  db.push(event);
+  res.status(200).send();
 });
 
 app.listen(port, () => {
